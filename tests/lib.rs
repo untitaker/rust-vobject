0c673cedcf9c950548c9b5368a674eff6afb3515
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-11-08 17:30:26 +0100

    Line continuations WIP

--- tests/lib.rs
+++ tests/lib.rs
@@ -32,3 +32,19 @@ fn test_wikipedia_1() {
     assert_eq!(tel_values.next().unwrap(), s!("(0221) 1234567"));
     assert!(tel_values.next().is_none());
 }
+
+#[test]
+fn test_line_cont() {
+    let item = parse_item(s!(
+        "BEGIN:VCARD\n\
+        VERSION:2.1\n\
+        N;ENCODING=QUOTED-PRINTABLE:Nikdo;Nikdo=\n\t\
+        vic\n\
+        NOTE:This ends with equal sign=\n\
+        TEL;WORK:5555\n \
+        4444\n\
+        END:VCARD")).unwrap();
+
+    assert_eq!(item.single_value(s!("TEL")), Some(s!("55554444")));
+    assert_eq!(item.single_value(s!("N")), Some(s!("Nikdo;Nikdo=vic")));
+}
