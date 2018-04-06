4a37644bbb0bd6486e820287cf9454398fe10733
Matthias Beyer <mail@beyermatthias.de>
2018-04-06 13:36:27 +0200

    Add ::from_raw() for helper types, make ::new() pub

--- src/util.rs
+++ src/util.rs
@@ -28,8 +28,12 @@ macro_rules! create_data_type {
         pub struct $name(String, $crate::param::Parameters);
 
         impl $name {
-            fn new(raw: String, params: $crate::param::Parameters) -> $name {
+            pub fn new(raw: String, params: $crate::param::Parameters) -> $name {
                 $name(raw, params)
+            }
+
+            pub fn from_raw(raw: String) -> $name {
+                $name(raw, BTreeMap::new())
             }
 
             pub fn raw(&self) -> &String {
