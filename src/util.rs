ace91d3c61c454f1dbb0c48d951a5abc7436a37c
Matthias Beyer <mail@beyermatthias.de>
2018-04-11 14:52:17 +0200

    Rewrite icalendar API for nice builder pattern style building of objects

--- src/util.rs
+++ src/util.rs
@@ -45,54 +45,6 @@ macro_rules! create_data_type {
     }
 }
 
-macro_rules! make_builder_fn {
-    (
-        fn $fnname:ident building $property_name:tt with_params,
-        $mapfn:expr => $( $arg_name:ident : $arg_type:ty ),*
-    ) => {
-        pub fn $fnname(mut self, params: $crate::param::Parameters, $( $arg_name : $arg_type ),*) -> Self {
-            let raw_value = vec![ $( $arg_name ),* ]
-                .into_iter()
-                .map($mapfn)
-                .collect::<Vec<_>>()
-                .join(";");
-
-            let prop = Property {
-                name: String::from($property_name),
-                params: params,
-                raw_value: raw_value,
-                prop_group: None
-            };
-
-            self.0.props.entry(String::from($property_name)).or_insert(vec![]).push(prop);
-            self
-        }
-    };
-
-    (
-        fn $fnname:ident building $property_name:tt,
-        $mapfn:expr => $( $arg_name:ident : $arg_type:ty ),*
-    ) => {
-        pub fn $fnname(mut self, $( $arg_name : $arg_type ),*) -> Self {
-            let raw_value = vec![ $( $arg_name ),* ]
-                .into_iter()
-                .map($mapfn)
-                .collect::<Vec<_>>()
-                .join(";");
-
-
-            let prop = Property {
-                name: String::from($property_name),
-                params: BTreeMap::new(),
-                raw_value: raw_value,
-                prop_group: None
-            };
-            self.0.props.entry(String::from($property_name)).or_insert(vec![]).push(prop);
-            self
-        }
-    }
-}
-
 #[cfg(feature = "timeconversions")]
 pub const DATE_TIME_FMT : &'static str = "%Y%m%dT%H%M%SZ";
 
