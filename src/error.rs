3dd9ceec1152d489bb72fbfbb506fc628baa1b1a
Ben Boeckel <mathstuf@gmail.com>
2020-04-08 22:45:18 -0400

    thiserror: replace usage of failure
    `thiserror` is much lighter and doesn't export a dependency on `failure`
    to consumers of the crate.

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,18 @@
-#[derive(Debug, Clone, Eq, PartialEq, Fail)]
+use thiserror::Error;
+
+#[derive(Debug, Clone, Error)]
 pub enum VObjectErrorKind {
-    #[fail(display = "Parser error: {}", _0)]
+    #[error("Parser error: {}", _0)]
     ParserError(String),
 
-    #[fail(display = "Not a Vcard")]
+    #[error("Not a Vcard")]
     NotAVCard,
 
-    #[fail(display = "Not a Icalendar: {}", _0)]
+    #[error("Not a Icalendar: {}", _0)]
     NotAnICalendar(String),
 
     #[cfg(feature = "timeconversions")]
-    #[fail(display = "{}", _0)]
+    #[error("{}", _0)]
     ChronoError(::chrono::format::ParseError),
 }
 
